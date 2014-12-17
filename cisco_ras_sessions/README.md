101cb805e31a0bccfaf3a459f0306fbef06a2867
markus weber <bumpemacvettn@googlemail.com>
2014-12-17 17:40:05 +0100

    added new plugin cisco_ras_sessions
    Description: This check returns the number of active sessions defined in Cisco Remote Access Monitoring MIB...

---
+++ cisco_ras_sessions/README.md
@@ -0,0 +1,16 @@
+# cisco_ras_sessions 
+This check returns the number of active sessions defined in Cisco Remote Access Monitoring MIB, these are currently the following: On layer 2 (PPTP, L2TP, L2F), layer 3 (IPsec) and layer 4 (SSL) virtual private networks. A service is only created if the "PeakConcurrentSessions" Counter of the desired protocol is greater then zero.
+
+# Installation
+* $ su - SITENAME
+* OMD[SITENAME]:~$ wget 'https://github.com/seppovic/check_mk-plugins/raw/master/cisco_ras_sessions/cisco_ras_sessions-1.0.mkp'
+* OMD[SITENAME]:~$ check_mk -P install cisco_ras_sessions-1.0.mkp
+
+# Requirements
+* A Cisco ASA or similar device.
+
+# Known issues
+* None so far.
+
+# History
+* 1.0 first release.
---
+++ cisco_ras_sessions/web/plugins/wato/check_parameters_netapp_cpoints.py
@@ -0,0 +1,59 @@
+checkgroups = []
+subgroup_networking = _("Networking")
+
+register_check_parameters(
+     subgroup_networking,
+    "cisco_ras_sessions",
+    _("Cisco Remote Access Sessions"),
+    Dictionary(
+        elements = [
+        (   "IPSec",
+            Tuple(
+            title = _("IPSec Tunnel"),
+            elements = [
+                Integer(title = _("Warning if more Sessions than: "), default_value = None),
+                Integer(title = _("Critical if more Sessions than: "), default_value = None)
+                ]
+            )
+        ),
+        (   "L2L",
+            Tuple(
+            title = _("Site to Site VPN"),
+            elements = [
+                Integer(title = _("Warning if more Sessions than: "), default_value = None),
+                Integer(title = _("Critical if more Sessions than: "), default_value = None)
+                ]
+            )
+        ),
+        (   "LB",
+        Tuple(
+            title = _("VPN load-balancing"),
+            elements = [
+                Integer(title = _("Warning if more Sessions than: "), default_value = None),
+                Integer(title = _("Critical if more Sessions than: "), default_value = None)
+                ]
+            )
+        ),
+        (   "SVC",
+            Tuple(
+            title = _("SSL VPN Client"),
+            elements = [
+                Integer(title = _("Warning if more Sessions than: "), default_value = None),
+                Integer(title = _("Critical if more Sessions than: "), default_value = None)
+                ]
+            )
+        ),
+        (   "WebVPN",
+        Tuple(
+            title = _("Clientless SSL VPN"),
+            elements = [
+                Integer(title = _("Warning if more Sessions than: "), default_value = None),
+                Integer(title = _("Critical if more Sessions than: "), default_value = None)
+                ]
+            )
+        ),
+        ],
+    ),
+    None,
+    "first"
+)
