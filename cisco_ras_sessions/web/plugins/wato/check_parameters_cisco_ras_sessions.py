9a8e60781ead22a539abb47e713e9dc0d145ddf2
markus weber <bumpemacvettn@googlemail.com>
2015-08-04 17:05:39 +0200

    fixed cisco_ras_sessions WATO config accordingly.

--- cisco_ras_sessions/web/plugins/wato/check_parameters_cisco_ras_sessions.py
+++ cisco_ras_sessions/web/plugins/wato/check_parameters_cisco_ras_sessions.py
@@ -7,53 +7,53 @@ register_check_parameters(
     _("Cisco Remote Access Sessions"),
     Dictionary(
         elements = [
-        (   "IPSec",
-            Tuple(
+            (   "IPSec",
+        Tuple(
             title = _("IPSec Tunnel"),
             elements = [
                 Integer(title = _("Warning if more Sessions than: "), default_value = None),
                 Integer(title = _("Critical if more Sessions than: "), default_value = None)
-                ]
-            )
-        ),
-        (   "L2L",
-            Tuple(
+            ]
+        )
+            ),
+            (   "L2L",
+        Tuple(
             title = _("Site to Site VPN"),
             elements = [
                 Integer(title = _("Warning if more Sessions than: "), default_value = None),
                 Integer(title = _("Critical if more Sessions than: "), default_value = None)
-                ]
-            )
-        ),
-        (   "LB",
+            ]
+        )
+            ),
+            (   "LB",
         Tuple(
             title = _("VPN load-balancing"),
             elements = [
                 Integer(title = _("Warning if more Sessions than: "), default_value = None),
                 Integer(title = _("Critical if more Sessions than: "), default_value = None)
-                ]
-            )
-        ),
-        (   "SVC",
-            Tuple(
+            ]
+        )
+            ),
+            (   "SVC",
+        Tuple(
             title = _("SSL VPN Client"),
             elements = [
                 Integer(title = _("Warning if more Sessions than: "), default_value = None),
                 Integer(title = _("Critical if more Sessions than: "), default_value = None)
-                ]
-            )
-        ),
-        (   "WebVPN",
+            ]
+        )
+            ),
+            (   "WebVPN",
         Tuple(
             title = _("Clientless SSL VPN"),
             elements = [
                 Integer(title = _("Warning if more Sessions than: "), default_value = None),
                 Integer(title = _("Critical if more Sessions than: "), default_value = None)
-                ]
-            )
-        ),
+            ]
+        )
+            ),
         ],
     ),
     None,
-    "first"
+    match_type = "dict",
 )
