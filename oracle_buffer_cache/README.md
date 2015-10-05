8476a7f38d2ee8cec14362da31d5492148520f9b
markus weber <bumpemacvettn@googlemail.com>
2015-10-05 13:57:53 +0200

    added new plugin oracle_buffer_cache.

---
+++ oracle_buffer_cache/README.md
@@ -0,0 +1,20 @@
+# oracle_buffer_cache
+This check monitors the buffer cache hit ratio in an ORACLE database instance.
+
+# Installation
+* $ su - SITENAME
+* OMD[SITENAME]:~$ wget 'https://github.com/seppovic/check_mk-plugins/raw/master/oracle_buffer_cache/oracle_buffer_cache-1.0.mkp'
+* OMD[SITENAME]:~$ check_mk -P install oracle_buffer_cache-1.0.mkp
+
+# Requirements
+* mk_oracle plugin installed, configured and patched with this patch:
+ https://github.com/seppovic/check_mk-plugins/raw/master/oracle_buffer_cache/agents/plugins/mk_oracle.patch
+* An Oracle Database
+
+# TODO
+
+# Known issues
+
+# History
+* 1.0   first release
+
---
+++ oracle_buffer_cache/web/plugins/wato/check_parameters_oracle_buffer_cache.py
@@ -0,0 +1,32 @@
+checkgroups = []
+subgroup_applications = _("Applications, Processes & Services")
+
+register_check_parameters(
+    subgroup_applications,
+    "entropy_avail",
+    _("Oracle buffer Cache"),
+    Dictionary(
+        help = _("The first time an Oracle Database user process requires a "
+                 "particular piece of data, it searches for the data in the "
+                 "database buffer cache. If the process finds the data already "
+                 "in the cache (a cache hit), it can read the data directly "
+                 "from memory. If the process cannot find the data in the cache "
+                 "(a cache miss), it must copy the data block from a datafile "
+                 "on disk into a buffer in the cache before accessing the data."
+                 "Accessing data through a cache hit is faster than data access "
+                 "through a cache miss."),
+        elements = [
+            ( "levels",
+                Tuple(
+                title = _("Minimum cache hit ratio needed, in percent"),
+                elements = [
+                    Percentage(title = _("Warning at"), default_value = 0.0, unit = _("% left")),
+                    Percentage(title = _("Critical at"), default_value = 0.0, unit = _("% left")),
+                ]
+                )
+            ),
+        ],
+    ),
+    None,
+    match_type = "dict",
+)
